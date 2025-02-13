d07910bb7c3ab4cca7ad7c76293319cf5960a614
LoganLui <leon199206@gmail.com>
2025-02-13 21:52:32 +0800

    增加 entity CoreMsg 用在 login 功能中

---
+++ src/main/java/win/TIA202/www/core/pojo/CoreMsg.java
@@ -0,0 +1,23 @@
+package win.TIA202.www.core.pojo;
+
+public class CoreMsg {
+	private boolean successfully;
+	private String message;
+	
+	public boolean isSuccessfully() {
+		return successfully;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setSuccessfully(boolean successfully) {
+		this.successfully = successfully;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
