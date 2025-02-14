3ef2c57d67f8ccbd6dcd18e050ea37195a341d2b
EthanLong-dev <ethanlong41152@gmail.com>
2025-02-14 20:32:34 +0800

    update the package location of componentScan in Spring configurations

--- src/main/java/win/TIA202/www/core/config/SpringConfig.java
+++ src/main/java/win/TIA202/www/core/config/SpringConfig.java
@@ -1,10 +1,5 @@
 package win.TIA202.www.core.config;
 
-import java.util.Properties;
-
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
 import org.hibernate.SessionFactory;
 import org.hibernate.dialect.MySQLDialect;
 import org.springframework.context.annotation.Bean;
@@ -18,38 +13,42 @@ import org.springframework.orm.hibernate5.SpringSessionContext;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @Configuration
-@ComponentScan({ "web.*.*.*.impl", "web.*.*.impl", "core.aspect" })
+@ComponentScan({"win.TIA202.www.web.*.*.impl", "win.TIA202.www.web.*.impl", "win.TIA202.www.core.aspect"})
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
 public class SpringConfig {
 
-	@Bean
-	public DataSource dataSource() throws IllegalArgumentException, NamingException {
-		JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
-		bean.setResourceRef(true);
-		bean.setJndiName("jdbc/NGFW");
-		bean.afterPropertiesSet();
-		return (DataSource) bean.getObject();
-	}
+    @Bean
+    public DataSource dataSource() throws IllegalArgumentException, NamingException {
+        JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+        bean.setResourceRef(true);
+        bean.setJndiName("NGFW");
+        bean.afterPropertiesSet();
+        return (DataSource) bean.getObject();
+    }
 
-	@Bean
-	public SessionFactory sessionFactory() throws IllegalArgumentException, NamingException {
-		return new LocalSessionFactoryBuilder(dataSource()).scanPackages("web.*.entity", "web.entity")
-				.addProperties(getHibernateProperties()).buildSessionFactory();
-	}
+    @Bean
+    public SessionFactory sessionFactory() throws IllegalArgumentException, NamingException {
+        return new LocalSessionFactoryBuilder(dataSource()).scanPackages("win.TIA202.www.web.*.entity", "win.TIA202.www.web.entity")
+            .addProperties(getHibernateProperties()).buildSessionFactory();
+    }
 
-	private Properties getHibernateProperties() {
-		Properties properties = new Properties();
-		properties.setProperty("hibernate.dialect", MySQLDialect.class.getName());
-		properties.setProperty("hibernate.show_sql", "true");
-		properties.setProperty("hibernate.format_sql", "true");
-		properties.setProperty("hibernate.current_session_context_class", SpringSessionContext.class.getName());
-		return properties;
-	}
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", MySQLDialect.class.getName());
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.current_session_context_class", SpringSessionContext.class.getName());
+        return properties;
+    }
 
-	@Bean	// 託管交易管理器
-	public TransactionManager transactionManager() throws IllegalArgumentException, NamingException {
-		return new HibernateTransactionManager(sessionFactory());
-	}
+    @Bean    // 託管交易管理器
+    public TransactionManager transactionManager() throws IllegalArgumentException, NamingException {
+        return new HibernateTransactionManager(sessionFactory());
+    }
 }
