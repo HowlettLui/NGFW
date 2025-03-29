d9c82e4fd06faa56809e61aa90488789773439bd
EthanLong-dev <ethanlong41152@gmail.com>
2025-03-30 04:37:19 +0800

    1. 新增entity

--- src/main/java/win/TIA202/www/web/entity/RequestDataAndUser.java
+++ src/main/java/win/TIA202/www/web/entity/RequestDataAndUser.java
@@ -1,4 +1,15 @@
 package win.TIA202.www.web.entity;
 
-public class RequestDataAndUser {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class RequestDataAndUser implements Serializable {
+    private static final long serialVersionUID = 7984750631792179395L;
+
+    private User requestData;
+    private User user;
 }
